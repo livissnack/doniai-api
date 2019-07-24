3ea28a47fc0a112cd69ca15e45a708cc4ebcdd4c
guan xin <guanxin@tinytiger.cn>
2019-07-24 18:21:15 +0800

    修改工厂数据工具

--- database/factory.js
+++ database/factory.js
@@ -14,6 +14,8 @@
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
 const Hash = use('Hash')
+const { formatDate } = require("../app/Utils/Helpers");
+
 
 Factory.blueprint('App/Models/User', (faker) => {
     return {
@@ -22,11 +24,11 @@ Factory.blueprint('App/Models/User', (faker) => {
         email: faker.email(),
         phone: faker.phone({ country: 'zh', mobile: true }),
         password: Hash.make( faker.string({ pool: 'abcd' }) ),
-        intro: faker.paragraph(),
+        intro: faker.sentence(),
         githup: faker.url(),
         sina: faker.url(),
         access_nums: faker.integer({ min: 0, max: 10000000 }),
-        empiric_value: faker.integer(),
+        empiric_value: faker.integer({ min: 0, max: 100000 }),
         status: faker.integer({ min: 0, max: 1 }),
         type: faker.integer({ min: 0, max: 2 }),
         website: faker.domain(),
@@ -45,7 +47,7 @@ Factory.blueprint('App/Models/Article', (faker) => {
         tag_id: faker.integer({ min: 1, max: 30 }),
         content: faker.paragraph(),
         image: faker.url({extensions: ['jpg', 'png']}),
-        publish_at: faker.timestamp()
+        publish_at: formatDate(new Date())
     }
 })
 
@@ -60,14 +62,14 @@ Factory.blueprint('App/Models/Comment', (faker) => {
         pid: faker.integer({ min: 1, max: 1000 }),
         type: faker.integer({ min: 1, max: 2 }),
         user_id: faker.integer({ min: 1, max: 100 }),
-        content: faker.paragraph(),
+        content: faker.sentence(),
     }
 })
 
 
 Factory.blueprint('App/Models/ArticleType', (faker) => {
     return {
-        value: faker.word(),
+        value: faker.unique(faker.state, 5),
     }
 })
 
@@ -75,10 +77,10 @@ Factory.blueprint('App/Models/Video', (faker) => {
     return {
         course_id: faker.integer({ min: 1, max: 11 }),
         name: faker.name(),
-        intro: faker.paragraph(),
+        intro: faker.sentence(),
         image: faker.url({extensions: ['jpg', 'png']}),
         duration: faker.minute(),
-        publish_at: faker.timestamp(),
+        publish_at: formatDate(new Date()),
         url: faker.url({extensions: ['mp4', 'avi', 'flv']}),
         status: faker.integer({ min: 0, max: 1 }),
         is_free: faker.integer({ min: 0, max: 1 }),
@@ -88,7 +90,7 @@ Factory.blueprint('App/Models/Video', (faker) => {
 Factory.blueprint('App/Models/Course', (faker) => {
     return {
         title: faker.sentence(),
-        intro: faker.paragraph(),
+        intro: faker.sentence(),
         image: faker.url({extensions: ['jpg', 'png']}),
         discuss_qq_group: faker.string({ pool: '499324462' }),
         support_wechat: faker.string({ pool: 'wei_23239231321' }),
@@ -112,19 +114,19 @@ Factory.blueprint('App/Models/Question', (faker) => {
     }
 })
 
-Factory.blueprint('App/Models/Reply', (faker) => {
+Factory.blueprint('App/Models/Replay', (faker) => {
     return {
         qid: faker.integer({ min: 1, max: 100 }),
         pid: faker.integer({ min: 1, max: 100 }),
         user_id: faker.integer({ min: 1, max: 100000 }),
-        content: faker.paragraph(),
+        content: faker.sentence(),
     }
 })
 
 Factory.blueprint('App/Models/Book', (faker) => {
     return {
         name: faker.name(),
-        intro: faker.paragraph(),
+        intro: faker.sentence(),
         image: faker.url({extensions: ['jpg', 'png']}),
         is_recommend: faker.integer({ min: 0, max: 1 }),
     }
@@ -143,8 +145,8 @@ Factory.blueprint('App/Models/BlackList', (faker) => {
     return {
         ip: faker.ip(),
         status: faker.integer({ min: 1, max: 10 }),
-        release_start_time: faker.timestamp(),
-        release_end_time: faker.timestamp(),
+        release_start_time: formatDate(new Date()),
+        release_end_time: formatDate(new Date()),
     }
 })
 
