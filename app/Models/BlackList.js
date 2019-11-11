24e147199ac8493ac96ded2e5dff41f57eef8f2b
guan xin <guanxin@tinytiger.cn>
2019-11-11 11:09:19 +0800

    submit

--- app/Models/BlackList.js
+++ app/Models/BlackList.js
@@ -2,16 +2,7 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
-const { formatDate } = require('../Utils/Helpers')
 
-class BlackList extends Model {
-  getReleaseStartTime(value) {
-    return formatDate(value)
-  }
-
-  getReleaseEndTime(value) {
-    return formatDate(value)
-  }
-}
+class BlackList extends Model {}
 
 module.exports = BlackList
