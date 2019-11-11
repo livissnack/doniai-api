2a8589c86bc7b682ab7f9826e9390a85a9ebbdc4
guan xin <guanxin@tinytiger.cn>
2019-11-11 11:35:59 +0800

    submit

--- app/Models/BlackList.js
+++ app/Models/BlackList.js
@@ -2,7 +2,24 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const moment = require('moment')
 
-class BlackList extends Model {}
+class BlackList extends Model {
+  static get computed() {
+    return ['status_text']
+  }
+
+  getReleaseStartTime(value) {
+    return moment(value).format('YYYY-MM-DD HH:mm:ss')
+  }
+
+  getReleaseEndTime(value) {
+    return moment(value).format('YYYY-MM-DD HH:mm:ss')
+  }
+
+  getStatusText({ status }) {
+    return status === 0 ? '放行' : '禁止访问'
+  }
+}
 
 module.exports = BlackList
--- app/Models/HspxsteelNew.js
+++ app/Models/HspxsteelNew.js
@@ -3,6 +3,19 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-class HspxsteelNew extends Model {}
+class HspxsteelNew extends Model {
+  static get computed() {
+    return ['type_text', 'status_text']
+  }
+
+  getTypeText({ type }) {
+    const mapText = ['未知', '行业新闻', '品信新闻']
+    return mapText[type]
+  }
+
+  getStatusText({ status }) {
+    return status === 0 ? '草稿' : '发布'
+  }
+}
 
 module.exports = HspxsteelNew
